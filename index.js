6adefb37d0963f1c4799da5505edfade26f11627
IFQ2017-02 <52657386+IFQ2017-02@users.noreply.github.com>
2019-07-10 12:38:42 +0200

    HTML hinzufügen

--- index.js
+++ index.js
@@ -1,14 +1,19 @@
 sap.ui.define([
 	"sap/m/Text",
+	"sap/ui/core/HTML",
 	"sap/m/Link"
 
-], function (Text, Link) {
+], function (Text, HTML, Link) {
 	"use strict";
 
 	new Text({
 		text: "Hello World"
 	}).placeAt("content");
 
+	new HTML({
+		content: "<br><br>"
+	}).placeAt("content");
+	
 	new Link({
 		text: "Auf zu Google!",
 		href: "https://google.de/"

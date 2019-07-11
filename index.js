29a556d11cf17251be35fb5928257451ec8d5ed6
IFQ2017-02 <ifq2017-02@cloud.bs-hassfurt.de>
2019-07-11 12:52:49 +0200

    Erweiterung XMLView

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 sap.ui.define([
 	"sap/m/Text",
 	"sap/ui/core/HTML",
-	"sap/m/Link"
+	"sap/m/Link",
+	"sap/ui/core/mvc/XMLView"
 
-], function (Text, HTML, Link) {
+], function (Text, HTML, Link, XMLView) {
 	"use strict";
 
 	new Text({
@@ -18,5 +19,15 @@ sap.ui.define([
 		text: "Auf zu Google!",
 		href: "https://google.de/"
 	}).placeAt("content");
-		
+	
+	new HTML({
+		content: "<br><br>"
+	}).placeAt("content");
+
+	XMLView.create({
+		viewName: "hts.ifq2017-02.walkthrough.view.App"
+	}).then(function (oView) {
+		oView.placeAt("content");
+	});
+	
 });

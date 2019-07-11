3c6c12ea823e92fe3c9febec7731f072900a8953
IFQ2017-02 <ifq2017-02@cloud.bs-hassfurt.de>
2019-07-11 14:15:06 +0200

    Controller hinzufügen

---
+++ controller/App.controller.js
@@ -0,0 +1,10 @@
+sap.ui.define([
+    "sap/ui/core/mvc/Controller"
+ ], function (Controller) {
+    "use strict";
+    return Controller.extend("hts.ifq2017-02.walkthrough.controller.App", {
+        onDrueckMich : function () {
+           alert("Guten Tag Hunz und Kunz");
+        }
+     });
+ });

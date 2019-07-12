555ac409b2c242c5136c35da595a87cc324dd2d3
IFQ2017-02 <ifq2017-02@cloud.bs-hassfurt.de>
2019-07-12 08:23:00 +0200

    Bugfix MessageToast

--- controller/App.controller.js
+++ controller/App.controller.js
@@ -1,14 +1,14 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
     "sap/m/MessageToast"
- ], function (Controller) {
+ ], function (Controller, MessageToast) {
     "use strict";
     return Controller.extend("hts.ifq2017-02.walkthrough.controller.App", {
         onAlert: function () {
            alert("Dies ist eine Standard-Meldung");
         },
-         onMessageToast: function () {
-            MessageToast.show("Dies ist ein MessageToast");
+        onMessageToast: function () {
+           MessageToast.show("Dies ist ein MessageToast");
       }
    });
  });

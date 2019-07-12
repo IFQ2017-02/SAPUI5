f34afba9da4c91adcf4a457decaddd8db86ac64e
IFQ2017-02 <ifq2017-02@cloud.bs-hassfurt.de>
2019-07-12 08:29:49 +0200

    Hinzufügen MessageBox

--- controller/App.controller.js
+++ controller/App.controller.js
@@ -1,14 +1,18 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller",
-    "sap/m/MessageToast"
- ], function (Controller, MessageToast) {
-    "use strict";
-    return Controller.extend("hts.ifq2017-02.walkthrough.controller.App", {
-        onAlert: function () {
-           alert("Dies ist eine Standard-Meldung");
-        },
-        onMessageToast: function () {
-           MessageToast.show("Dies ist ein MessageToast");
+   "sap/ui/core/mvc/Controller",
+   "sap/m/MessageToast",
+   "sap/m/MessageBox"
+], function (Controller, MessageToast, MessageBox) {
+   "use strict";
+   return Controller.extend("hts.ifq2017-02.walkthrough.controller.App", {
+      onAlert: function () {
+         alert("Dies ist eine Standard-Meldung");
+      },
+      onMessageToast: function () {
+         MessageToast.show("Dies ist ein MessageToast");
+      },
+      onMessageBox: function () {
+         MessageBox.show("Dies ist eine SAPUI5-Meldung");
       }
    });
- });
+});

dd0507f2c310e3c46687fb4725924f0dac9af3ec
IFQ2017-02 <ifq2017-02@cloud.bs-hassfurt.de>
2019-07-12 08:20:39 +0200

    Hinzufügen MessageToast

--- controller/App.controller.js
+++ controller/App.controller.js
@@ -1,10 +1,14 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
+    "sap/ui/core/mvc/Controller",
+    "sap/m/MessageToast"
  ], function (Controller) {
     "use strict";
     return Controller.extend("hts.ifq2017-02.walkthrough.controller.App", {
-        onDrueckMich: function () {
-           alert("Guten Tag Hunz und Kunz");
-        }
-     });
+        onAlert: function () {
+           alert("Dies ist eine Standard-Meldung");
+        },
+         onMessageToast: function () {
+            MessageToast.show("Dies ist ein MessageToast");
+      }
+   });
  });

0cab2251a5f816a664dc42c5aebd4d3fc0a40006
IFQ2017-02 <ifq2017-02@cloud.bs-hassfurt.de>
2019-07-18 12:13:37 +0200

    Komplett-Erweiterung auf Step 15

--- index.js
+++ index.js
@@ -1,33 +1,13 @@
 sap.ui.define([
-	"sap/m/Text",
-	"sap/ui/core/HTML",
-	"sap/m/Link",
-	"sap/ui/core/mvc/XMLView"
-
-], function (Text, HTML, Link, XMLView) {
+	"sap/ui/core/ComponentContainer"
+], function (ComponentContainer) {
 	"use strict";
 
-	new Text({
-		text: "Hello World"
+	new ComponentContainer({
+		name: "sap.ui.demo.walkthrough",
+		settings : {
+			id : "walkthrough"
+		},
+		async: true
 	}).placeAt("content");
-
-	new HTML({
-		content: "<br><br>"
-	}).placeAt("content");
-	
-	new Link({
-		text: "Auf zu Google!",
-		href: "https://google.de/"
-	}).placeAt("content");
-	
-	new HTML({
-		content: "<br><br>"
-	}).placeAt("content");
-
-	XMLView.create({
-		viewName: "hts.ifq2017-02.walkthrough.view.App"
-	}).then(function (oView) {
-		oView.placeAt("content");
-	});
-	
 });
